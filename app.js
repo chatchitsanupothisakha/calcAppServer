d3f73324cff7ef151f85892acffc696d5cce387e
Chatchitsanu Pothisakha <chatchitsanu@gmail.com>
2018-08-26 22:43:09 +0700

    remove code

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fs = require('fs');
 const MongoClient = require('mongodb').MongoClient;
-// const url = 'mongodb://heroku_t6bp9lvd:rdoamshc7bbqqhq0kc0mjhdm40@ds133622.mlab.com:33622/heroku_t6bp9lvd';
 const url = process.env.MONGODB_URI
 const dbName = 'heroku_t6bp9lvd';
 const collection = 'UserData';
@@ -13,13 +11,6 @@ app.use(bodyParser.json());
 app.use(cors())
 
 app.get('/api/user', function(req, res) {
-    // fs.readFile('UID-' + req.query.id + '.json', function(err, file) {
-    //     if (err) {
-    //         res.send({ err: true });
-    //     } else {
-    //         res.send(JSON.parse(file))
-    //     }
-    // });
     MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
         const db = client.db(dbName);
         db.collection(collection).findOne({ _id: req.query._id }, function(err, data) {
@@ -33,13 +24,6 @@ app.get('/api/user', function(req, res) {
     });
 });
 app.post('/api/user', function(req, res) {
-    // fs.writeFile('UID-' + req.body.id + '.json', JSON.stringify(req.body), function(err) {
-    //     if (err) {
-    //         res.send(err)
-    //     } else {
-    //         res.send('save complete!');
-    //     }
-    // });
     MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
         const db = client.db(dbName);
         db.collection(collection).save(req.body, function(err, r) {
