4eb2ef7600f56876595ddf66449bd99826e29585
Chatchitsanu Pothisakha <chatchitsanu@gmail.com>
2018-08-26 19:30:36 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const AWS = require("aws-sdk");
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors())
+
+AWS.config.update({
+    region: "ap-southeast-1",
+    endpoint: "https://dynamodb.ap-southeast-1.amazonaws.com",
+    accessKeyId: 'AKIAIA3VB42566QX6O3A',
+    secretAccessKey: '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'
+});
+var docClient = new AWS.DynamoDB.DocumentClient();
+var table = "user";
+
+app.get('/api/user', function(req, res) {
+    var params = {
+        TableName: table,
+        Key: {
+            "id": req.query.id
+        }
+    };
+    docClient.get(params, function(err, data) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(data);
+        }
+    });
+});
+app.post('/api/user', function(req, res) {
+    var params = {
+        TableName: table,
+        Item: req.body
+    };
+    docClient.put(params, function(err, data) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send("Complete");
+        }
+    });
+});
+app.listen(process.env.port || 8081, () => console.log('app run!'))
